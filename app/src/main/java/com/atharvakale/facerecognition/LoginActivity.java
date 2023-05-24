04f98c2ad24ae512ca6650c491c358393b8598f2
thanhdien02 <106503650+thanhdien02@users.noreply.github.com>
2023-05-24 14:23:03 +0700

    UI

---
+++ app/src/main/java/com/atharvakale/facerecognition/LoginActivity.java
@@ -0,0 +1,29 @@
+package com.atharvakale.facerecognition;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    Button btn_login;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        btn_login = findViewById(R.id.login_button);
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+
+                startActivity(intent);
+            }
+        });
+    }
+}
